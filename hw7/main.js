a229b270c31d1abbb5b012a9ae506d4a37dbfb04
Vladislav <kvlad569@gmail.com>
2024-02-02 13:19:55 +0200

    new

---
+++ hw7/main.js
@@ -0,0 +1,7 @@
+const number1 = (prompt("Введіть перше число:"));
+const number2 = (prompt("Введіть друге число:"));
+const number3 = (prompt("Введіть третє число:"));
+
+const average = (+number1 + +number2 + +number3) / 3;
+
+alert ("Середнє арифметичне: " + average);
