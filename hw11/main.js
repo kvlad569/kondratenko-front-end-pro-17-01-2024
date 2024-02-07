a2ca5cd500b74fe38465d6cc4fcf8250fd795bcc
Vladislav <kvlad569@gmail.com>
2024-02-07 18:24:06 +0200

    new

--- hw11/main.js
+++ hw11/main.js
@@ -1,11 +1,11 @@
 function removeElement(array, item) {
     const index = array.indexOf(item);
     if (index !== -1) {
-        array.splice(index, 2);
+        array.splice(index, 1);
     }
 }
 
 const array = [10, 20, 30, 40, 50, 60, 70];
 removeElement(array, 30);
 console.log(array);  
-// [10, 20, 50, 60, 70] извлекли 30 и 40 так как индекс 2 
+// [10, 20, 50, 60, 70] извлекли 30 
