e5edf6945dd9f83c87dff9f40eccd242c899447a
Vladyslav <69350775+kvlad569@users.noreply.github.com>
2024-02-07 17:43:44 +0200

    Update main.js

--- hw11/main.js
+++ hw11/main.js
@@ -8,4 +8,4 @@ function removeElement(array, item) {
 const array = [10, 20, 30, 40, 50, 60, 70];
 removeElement(array, 30);
 console.log(array);  
-// [10, 20, 50, 60, 70] извлекли 30 и 40 так как индекс 2 
+// [10, 20, 50, 60, 70] извлекли 30 и 40 так как (index, 2) 
