1e4a3aa58ef17c86c1224de44339e3252b262ce0
Vladislav <kvlad569@gmail.com>
2024-03-12 15:31:50 +0200

    new

---
+++ hw21/main.js
@@ -0,0 +1,10 @@
+
+var table = document.getElementById("myTable");
+var count = 1;
+for (var i = 0; i < 10; i++) {
+  var row = table.insertRow();
+  for (var j = 0; j < 10; j++) {
+    var cell = row.insertCell();
+    cell.innerHTML = count++;
+  }
+}
