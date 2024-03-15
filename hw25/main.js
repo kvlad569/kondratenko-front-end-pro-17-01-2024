b30f70bdd3e70f144c4c3d60d7495c306f85b651
Vladislav <kvlad569@gmail.com>
2024-03-15 20:02:47 +0200

    new

--- hw25/main.js
+++ hw25/main.js
@@ -10,6 +10,6 @@ function generateList(array) {
     return html;
 }
 
-const array = [1, 2, 3, [1.1, 1.2, 1.3], 4, [2.1, 2.2, 2.3], 5, [3.1, 3.2, 3.3], 6];
+const array = [1, 2, [1.1, 1.2, 1.3], 3, 4, [2.1, 2.2, 2.3], 5, [3.1, 3.2, 3.3], 6];
 const outputDiv = document.getElementById('output');
 outputDiv.innerHTML = generateList(array);
