ef85fecee3fa95298481a0aec7f810e03ed2ab6d
Vladislav <kvlad569@gmail.com>
2024-03-19 16:34:17 +0200

    new

---
+++ hw27/main.js
@@ -0,0 +1,27 @@
+function displayData() {
+    const form = document.getElementById("registrationForm");
+    const dataDisplay = document.getElementById("dataDisplay");
+    const registrationTitle = document.getElementById("registrationTitle");
+
+    let table = "<h2>Дані, введені користувачем:</h2>";
+    table += "<table border='1'>";
+    table += "<tr><th>Поле</th><th>Значення</th></tr>";
+    
+    for (let i = 0; i < form.elements.length; i++) {
+        const field = form.elements[i];
+        if (field.type === "checkbox" || field.type === "radio") {
+            if (field.checked) {
+                table += "<tr><td>" + field.labels[0].innerText + "</td><td>Так</td></tr>";
+            }
+        } else {
+            if (field.value.trim() !== "") {
+                table += "<tr><td>" + field.labels[0].innerText + "</td><td>" + field.value + "</td></tr>";
+            }
+        }
+    }
+    
+    table += "</table>";
+    dataDisplay.innerHTML = table;
+    registrationTitle.style.display = "none"; 
+    form.style.display = "none"; 
+}
