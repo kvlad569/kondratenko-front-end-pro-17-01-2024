b0572315114fe588836e84f035d8f13133b0c202
Vladislav <kvlad569@gmail.com>
2024-03-20 14:55:40 +0200

    new

--- hw26/main.js
+++ hw26/main.js
@@ -56,11 +56,33 @@ const products = {
          image: 'images/perfume/men perfume/tovar3/1.png', 
          description: 'Якщо ваш обранець віддає перевагу фужерним ароматам, насиченими східно-деревними переливами' }
     ],
-    womenPerfume: [{ name: 'Товар 1', 
-    image: 'images/perfume/men perfume/tovar1/1.jpg', 
-    description: 'Описание товара 1' }],
-    
-    unisexPerfume: [{ name: 'Товар 7', image: 'product7.jpg', description: 'Описание товара 7' }],
+    womenPerfume: [
+        { name: 'Calvin Klein CK IN2U Her', 
+        image: 'images/perfume/women perfume/tovar1/1.jpg', 
+        description: 'Початкова нота: Бергамот, Грейпфрут, Листя чорної смородини. Яскравий CK IN2U Her - для легкої, спокусливої, квітучої жінки.' },
+
+        { name: 'Giorgio Armani My Way Floral', 
+        image: 'images/perfume/women perfume/tovar2/1.jpg', 
+        description: 'Початкова нота: Гіркий апельсин, Зелений мандарин, Квітка апельсина. Унікальний східно-квітковий аромат, створений спеціально для творчих, цілеспрямованих та впевнених у собі дівчат, які точно знають, чого хочуть від життя.' },
+
+        { name: 'Valentino Donna Born In Roma', 
+        image: 'images/perfume/women perfume/tovar3/1.jpg', 
+        description: 'Початкова нота: Бергамот, Чорна смородина. Східно-деревні аромати з домішками квіткових нот – це вишукане поєднання, виткане не тільки з професіоналізму його творців, але й відгомонів найвищих почуттів: кохання, ніжності, трепету .' }
+],
+    unisexPerfume: [
+        { name: 'Pascal Morabito Noir', 
+        image: 'images/perfume/unisex perfume/tovar1/1.jpg', 
+        description: 'Вишуканий аромат Noir уособлює уявлення французьких парфумерних майстрів бренда Pascal Morabito про загадкову східну ніч.' },
+
+        { name: 'Les Nereides Rue Paradis', 
+        image: 'images/perfume/unisex perfume/tovar2/1.jpg', 
+        description: 'ип аромату: квіткові, східні. Початкова нота: Зірчастий аніс. Нота серця: Мімоза. Кінцева нота: Сандал' },
+
+        { name: 'ТоваGiorgio Armani Si Passione', 
+        image: 'images/perfume/unisex perfume/tovar3/1.jpg', 
+        description: 'Для найжіночніших і прекрасних, які вже встигли оцінити і полюбити божественний аромат Giorgio Armani Si, вийшла ще більш пристрасніша його версія – парфумована вода Si Passione.' }
+    ],
+
     brows: [{ name: 'Товар 10', image: 'product10.jpg', description: 'Описание товара 10' }],
     lips: [{ name: 'Товар 13', image: 'product13.jpg', description: 'Описание товара 13' }],
     eyes: [{ name: 'Товар 16', image: 'product16.jpg', description: 'Описание товара 16' }],
